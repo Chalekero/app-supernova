a0afca494b9f52a8910808720ec803f490a88ed4
Chalekero <167400187+Chalekero@users.noreply.github.com>
2025-01-08 20:33:40 -0400

    Update numeros.js

--- numeros.js
+++ numeros.js
@@ -1,1 +1,32 @@
-
+[
+  {
+    "nombre": "Locutor 1",
+    "inicio": "06:00",
+    "fin": "08:00",
+    "whatsapp": "https://wa.me/1234567890",
+    "facebook": "https://facebook.com/locutor1",
+    "tiktok": "https://tiktok.com/locutor1",
+    "youtube": "https://youtube.com/locutor1",
+    "instagram": "https://instagram.com/locutor1"
+  },
+  {
+    "nombre": "Locutor 2",
+    "inicio": "08:00",
+    "fin": "10:00",
+    "whatsapp": "https://wa.me/1234567891",
+    "facebook": "https://facebook.com/locutor2",
+    "tiktok": "https://tiktok.com/locutor2",
+    "youtube": "https://youtube.com/locutor2",
+    "instagram": "https://instagram.com/locutor2"
+  },
+  {
+    "nombre": "Locutor 3",
+    "inicio": "10:00",
+    "fin": "13:00",
+    "whatsapp": "https://wa.me/1234567892",
+    "facebook": "https://facebook.com/locutor3",
+    "tiktok": "https://tiktok.com/locutor3",
+    "youtube": "https://youtube.com/locutor3",
+    "instagram": "https://instagram.com/locutor3"
+  }
+]
